de5e450bb927390a8d25ff9ef0d9bd479b655e0b
STEPASEM <94398631+STEPASEM@users.noreply.github.com>
2025-05-29 23:44:10 +0300

    [+] ChessBoard - Модуль создания поля (квадрат и ромб)

---
+++ Modules/СhessBoard.py
@@ -0,0 +1,45 @@
+from math import sqrt
+
+class ChessBoard:
+    def __init__(self, TypeBoard: int, SizeBoard: int):
+        self.TypeBoard = TypeBoard # 0 - прямоугольник, 1 - ромб
+        self.SizeBoard = SizeBoard # Кол-во клеточек (обязательно из числа
+                                   # должен извлекаться ровный квадратный корень)
+                                   # минимум 81
+        self.Board = self.CreateBoard()
+
+
+    def CreateBoard(self) -> list:
+        """Создание доски"""
+        Board = [[0 for _ in range(int(sqrt(self.SizeBoard)))] for _ in range(int(sqrt(self.SizeBoard)))]
+
+        if self.TypeBoard == 1:  # Если ромб
+            side = len(Board)
+            if side % 2 == 0:
+                center = (side - 1) / 2
+                radius = center
+
+                for i in range(side):
+                    for j in range(side):
+                        if abs(i - center) + abs(j - center) > radius + 0.5:
+                            Board[i][j] = 9
+
+            else:
+                center = (side - 1) // 2
+                radius = center + 1
+
+                for i in range(side):
+                    for j in range(side):
+                        if abs(i - center) + abs(j - center) > radius:
+                            Board[i][j] = 9
+
+        return Board
+
+
+    def PrintBoard(self):
+        """Печать доски"""
+        for i in self.Board:
+            print(" ".join(map(str, i)))
+
+# Для проверки:
+#ChessBoard(1, 36).PrintBoard()
