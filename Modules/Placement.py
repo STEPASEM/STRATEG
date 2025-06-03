18cbf869eec59d8bdbeb094d6732e7952df1f2ef
STEPASEM <94398631+STEPASEM@users.noreply.github.com>
2025-06-03 16:19:15 +0300

    [*] Добавлено хранение на каких ячейках чьи фигуры стоят, и также ограничение на постановку фигуры, что каждый ставит в своей части поля

--- Modules/Placement.py
+++ Modules/Placement.py
@@ -1,13 +1,14 @@
 from Modules.СhessBoard import ChessBoard
 
-
 class Placement:
-    def __init__(self, Board: ChessBoard, TypeFigure: int):
+    def __init__(self, Board: ChessBoard, players_figure: list[list[int]], TypeFigure: int, id_player: int):
         self.Board = Board
         self.matrix_board = [row[:] for row in Board.GetBoard()]
+        self.players_figure = players_figure
+        self.id_player = id_player
         self.TypeFigure = TypeFigure
 
-    def show_available_cells(self):
+    def show_available_cells(self) -> list:
         """Проверяет наличие доступных клеток."""
         available_cells = []
         side = self.Board.LengthBoard()
@@ -16,6 +17,21 @@ class Placement:
                 if self.matrix_board[i][j] == 0:
                     available_cells.append((i, j))
         return available_cells
+
+    def CheckZone(self, row) -> bool:
+        """Проверяет введенную координату на зону игрока она до середины поля по строкам."""
+        sider = self.Board.LengthBoard()
+        side = sider//2
+        fl = 0
+        if sider %2 == 0:
+            side-=1
+            fl = 1
+        if self.id_player ==  0:
+            return row<side
+        elif self.id_player == 1:
+            if fl == 1:
+                side+=1
+            return row>side
 
     def place_figure(self):
         """Размещает фигуру на доске в выбранной пользователем клетке."""
@@ -28,7 +44,8 @@ class Placement:
             try:
                 print("Введите координаты клетки (строка и столбец через пробел): ", end="")
                 row, col = map(int, input().split())
-                if (row, col) in available_cells:
+                if (row, col) in available_cells and self.CheckZone(row):
+                    self.players_figure[self.id_player].append((row, col))
                     self.matrix_board[row][col] = self.TypeFigure
                     print(f"Фигура {self.TypeFigure} успешно размещена на клетке ({row}, {col}).")
                     self.Board.Board = self.matrix_board
